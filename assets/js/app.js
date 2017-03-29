a0a517e31819b93a00bdbfefca87114fcd9b728d
tjking20 <tjking20@outlook.com>
2017-03-29 13:27:00 -0400

    finalized navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,27 @@
 $(document).ready(function(){
 	
+	//function scrolls to appropriate sections of page
+	$(document).on("click", ".nav-link", function(){
+	    var section = $(this).attr("href");
+	    console.log(section);
+	    console.log(this);
+	    $("html, body").animate({
+	        scrollTop: $(section).offset().top
+	    });
+	});
 
-		$(document).on("click", ".nav-link", function(){
-		    var section = $(this).attr("href");
-		    console.log(section);
-		    console.log(this);
-		    $("html, body").animate({
-		        scrollTop: $(section).offset().top
-		    });
-		});
+
+
+
+	//fades name on home page
+	$(window).scroll(function(){
+	    $("#home").css("opacity", 1 - 
+	    $(window).scrollTop() / 290);
+	});
+
 
 });
 
-$(window).scroll(function(){
-    $("#home").css("opacity", 1 - 
-    $(window).scrollTop() / 290);
-  });
 
 
-// $(document).ready(function(){		
-// 	var offset = 49;	
-// 	$(document).on("click", ".nav-link", function(){
-// 	    var section = $(this).attr("href");
-// 	    console.log(section);
-// 	    $("html, body").animate({
-// 	        scrollTop: $(section).offset().top - offset
-// 	    });
-// 	});
-// });
 
